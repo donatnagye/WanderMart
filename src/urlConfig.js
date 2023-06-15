02e2386488b46823418a1270e2eb9ac4c4aa1903
osho-20 <yashjainiiita@gmail.com>
2023-06-16 03:59:55 +0530

    user

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
-const backendBase = window.location.hostname === 'localhost' ? 'http://localhost:5000' : 'https://wandermartapi.onrender.com/';
+const backendBase = 'https://wandermartapi.onrender.com/';
 // const frontendBase = window.location.hostname === 'localhost' ? 'http://localhost:3000' : 'http://localhost:3000';
-
+// window.location.hostname === 'localhost' ? 'http://localhost:5000' :
 export const backend = `${backendBase}/api`;
 // export const frontendApi = `${frontendBase}`;
