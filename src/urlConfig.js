7e9b4edd128a62b5202e46f52e13effb1609f79d
osho-20 <yashjainiiita@gmail.com>
2023-06-16 04:20:44 +0530

    link corrected

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,8 @@
-const backendBase = 'https://wandermartapi.onrender.com/';
+const backendBase =
+	window.location.hostname === 'localhost'
+		? 'http://localhost:5000'
+		: 'https://wandermartapi.onrender.com';
 // const frontendBase = window.location.hostname === 'localhost' ? 'http://localhost:3000' : 'http://localhost:3000';
-// window.location.hostname === 'localhost' ? 'http://localhost:5000' :
+
 export const backend = `${backendBase}/api`;
 // export const frontendApi = `${frontendBase}`;
